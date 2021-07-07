63a1d1d69a8489331a8543bfd940dd7762277702
Nonso <nonsootoh@gmail.com>
2021-07-07 18:03:18 -0400

    structing mid comp

--- src/Components/ItemHeaderComp.jsx
+++ src/Components/ItemHeaderComp.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Btn from "./InteractiveComp/Btn";
 
+import companyLogo from "../imgs/logo-mastercraft.svg";
+
 const ItemHeaderComp = () => {
   return (
     <section className="itemHeaderComp">
+      <img
+        className="itemHeaderComp__Img"
+        src={companyLogo}
+        alt="company logo"
+      />
       <p className="itemHeader__Title">Mastercraft Bamboo Monitor Riser</p>
       <p className="itemHeader__descri">
         A beautiful & handcrafted monitor stand to reduce neck and eye strain.
