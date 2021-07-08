d4e210e53ed6b5f5907d44eefa2c9c42b1f86d3f
Nonso <nonsootoh@gmail.com>
2021-07-07 20:13:41 -0400

    Finshed Fund comp

--- src/Components/FundComp.jsx
+++ src/Components/FundComp.jsx
@@ -2,18 +2,23 @@ import React from "react";
 
 const FundComp = () => {
   return (
-    <section className="FundComp mainPageComp">
-      <div className="FundItems">
-        <p className="FundtItems__Header">$ 89000</p>
-        <p className="FundItem__subHead">of $100000 backed</p>
+    <section className="mainPageComp FundContanier">
+      <div className="FundComp">
+        <div className="FundItems">
+          <p className="FundtItems__Header">$89000</p>
+          <p className="FundItem__subHead">of $100000 backed</p>
+        </div>
+        <div className="FundItems">
+          <p className="FundtItems__Header">5000</p>
+          <p className="FundItem__subHead">total backers</p>
+        </div>
+        <div className="FundItems">
+          <p className="FundtItems__Header">56</p>
+          <p className="FundItem__subHead">days left</p>
+        </div>
       </div>
-      <div className="FundItems">
-        <p className="FundtItems__Header">5000</p>
-        <p className="FundItem__subHead">total backers</p>
-      </div>
-      <div className="FundItems">
-        <p className="FundtItems__Header">56</p>
-        <p className="FundItem__subHead">days left</p>
+      <div className="fundProgessContainer">
+        <div className="fundProgessBar"></div>
       </div>
     </section>
   );
